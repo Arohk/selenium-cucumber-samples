2d25b6f30b8336253a2e99251149b4ad8c9377a4
Nikolay Nedyalkov <nikolay_nedyalkov@starsgroup.com>
2021-07-02 10:44:17 +0300

    add base page method

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -1,14 +1,16 @@
 package core;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
-	private static final int TIMEOUT = 5;
+	private static final int TIMEOUT = 10;
 	private static final int POLLING = 100;
 
 	protected WebDriver driver;
@@ -20,15 +22,17 @@ public class BasePage {
 		PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
 	}
 
-	protected void waitForElementToAppear(By locator) {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	protected void waitForElementToAppear(WebElement locator) {
+		wait.until(ExpectedConditions.visibilityOf(locator));
 	}
 
-	protected void waitForElementToDisappear(By locator) {
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+	protected void waitForElementToBeClickable(WebElement locator) {
+		wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(locator)));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(locator);
+		actions.click(locator);
+		Action action = actions.build();
+		action.perform();
 	}
 
-	protected void waitForTextToDisappear(By locator, String text) {
-		wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, text)));
-	}
 }
