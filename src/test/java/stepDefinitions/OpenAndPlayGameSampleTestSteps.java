0dec5d9406e887864fd7f4a8a69fb3400c29116c
Nikolay Nedyalkov <nikolay_nedyalkov@starsgroup.com>
2021-07-02 10:56:43 +0300

    cleanup code

--- src/test/java/stepDefinitions/OpenAndPlayGameSampleTestSteps.java
+++ src/test/java/stepDefinitions/OpenAndPlayGameSampleTestSteps.java
@@ -1,9 +1,7 @@
 package stepDefinitions;
 
-import cucumber.api.java.it.Ma;
 import org.sikuli.script.FindFailed;
 import org.testng.Assert;
-import org.testng.reporters.jq.Main;
 import pages.HomePage;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
